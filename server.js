584b3dd8aaa0e1fbe308e4164fb8243ae0ef092b
mgravis39 <mgravi07@gmail.com>
2017-08-22 19:42:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+app.get('/article-one', function (req, res) {
+  res.send('Article One is requested and will be serverd');
+});
+
+app.get('/article-two', function (req, res) {
+  res.send('Article Two is requested and will be serverd');
+});
+
+app.get('/article-three', function (req, res) {
+  res.send('Article Three is requested and will be serverd');
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
