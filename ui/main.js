737f900d146f3c97c1f3444a590b1b0b44948492
mgravis39 <mgravi07@gmail.com>
2017-08-23 20:16:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,13 @@ var element = document.getElementById('index-class');
 element.innerHTML = 'New Text is being Loaded';
 
 // Move the Image
+var marginLeft = 0;
+function moveRight () {
+    marginLeft = marginLeft + 10;
+     img.style.marginLeft = marginLeft + 'px';
+}
 var img = document.getElementById('madi');
 img.onclick = function () {
     img.style.marginLeft = '50px';
+    var intervel = setInterval(moveRight, 100);
     };
