7656517493f30cf9e86001cf79b35205e543c5a9
mgravis39 <mgravi07@gmail.com>
2017-08-23 18:33:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,55 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles {
+    articleOne
+}
+
+var content = {
+    title: 'Article One | Ravikumar',
+    heading: 'Article One',
+    date: '23 Aug 2017',
+    content:  `Article One HTML Page contents comes here. Article One HTML Page contents comes here. Article One HTML Page contents comes here. Article One HTML Page contents comes here. `
+}
+
+function createTemplate(data) {
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    
+    var htmlTemplate = `{
+        <!doctype html>
+    <html>
+        <head>
+            <link href="/ui/style.css" rel="stylesheet" />
+            <title>${title}</title>
+            <meta name="viewport" content = "width=device-width, initial-scale=1"/>
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="center">
+                <img src="/ui/madi.png" class="img-medium"/>
+            </div>
+            <div> <a href = "/"> Home</a> </div>
+           <div class="container">
+            <hr>
+                <h3>${heading}</h3>
+                <div>${date}</div>
+                <br>
+                <div>
+                    <p>${content}</p>
+                </div>
+            <hr>
+            </div>
+            <script type="text/javascript" src="/ui/main.js">
+            </script>
+        </body>
+    </html>
+    }`;
+    return htmlTemplate;
+}
+
 app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
